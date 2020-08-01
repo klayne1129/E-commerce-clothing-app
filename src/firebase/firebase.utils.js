e76e65fa4a72500de1ab46b6b038771e04c489a9
Katie Layne <klayne1129@gmail.com>
2020-07-31 21:06:29 -0500

    implemented firebase utils, included ability to store authenticated users in firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,29 @@ const config = {
     appId: "1:815324630892:web:27963779c8355309a7ed49"
   }
 
+  export const createUserProfileDocument = async (userAuth,additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get()
+    
+    if(!snapShot.exists) {
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log('error creating user', error.message)
+      }
+    }
+    return userRef
+  }
+
   firebase.initializeApp(config)
 
   export const auth = firebase.auth()
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -15,7 +15,7 @@ class ShopPage extends Component {
         const {collections} = this.state
         return (
             <div className='shop-page'>
-               {collections.map(({id, ... otherCollectionProps}) => (
+               {collections.map(({id, ...otherCollectionProps}) => (
                    <CollectionPreview key={id} {...otherCollectionProps}/>
                ))}
             </div>
